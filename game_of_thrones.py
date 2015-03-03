31a77befba62e1639a3da996322b53f1f280dcae
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-03-03 12:23:22 +0100

    adding a new hackerrank puzzle, but just the problem statement and rough skeleton.

---
+++ game_of_thrones.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+'''
+	https://www.hackerrank.com/challenges/game-of-thrones
+
+	Robert ... needs a key that is an anagram of a certain palindrome string.	
+	The king has a string composed of lowercase English letters. Help him figure 
+	out if any anagram of the string can be a palindrome or not
+
+	Input Format 
+	A single line which contains the input string
+
+	Constraints 
+	1<=length of string <= 10^5 
+	Each character of the string is a lowercase English letter.
+
+	Output Format 
+	A single line which contains YES or NO in uppercase.
+
+	Anagram:
+	An anagram is a type of word play, the result of rearranging the letters of a word or phrase to produce 
+	a new word or phrase, using all the original letters exactly once; for example Torchwood can be rearranged 
+	into Doctor Who.
+
+	Palindrome:
+	A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or 
+	forward.
+'''
+
+def check_palindrome():
+	pass
+
+def check_anagram():
+	pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
