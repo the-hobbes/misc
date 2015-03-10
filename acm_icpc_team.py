136f2b8bf81ffb930edbf157655de2f7e4f50f66
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-03-10 17:18:53 +0100

    solution for the ACM problem on hackerrank that runs in a reasonable amount of time. I've added both a nested loops solution and a solution using itertools, and they seem to run about the same'

--- acm_icpc_team.py
+++ acm_icpc_team.py
@@ -23,7 +23,7 @@
 	Therefore:
 		person 1 and person 3 together know all 5 topics.
 		person 4 and person 4 together know all 5 topics.
-		The maximum number of topics a 2 person team can know is 5 in this case.
+		(The maximum number of topics a 2 person team can know is 5 in this case.)
 		Only 2 teams can achieve this. 
 		Thus, the output is:
 			5
@@ -35,38 +35,38 @@
 
 import itertools
 
-# def sum_string(string):
-# 	return sum(int(character) for character in string if character.isdigit())
+def compute_values_itertools(n, m, people):
+	format_string = '{0:0%sb}' % m
+	best_value, team_count = 0, 0
 
-def person_comparer(person_1, person_2):
-	# perform bitwise-or on two people, and return an integer array of the result
-	return [int(a) | int(b) for a,b in itertools.izip(person_1, person_2)]
-	
-def find_max_knowledge(n, m, people):
-	# compute bitwise comparison of every pair of teams
-	teams = {}
-	for item in itertools.combinations(people, 2):
-		teams[item] = person_comparer(item[0], item[1])
+	for combination in itertools.combinations(people, 2):
+		bitwise_or = int(combination[0], 2) | int(combination[1], 2)
+		count = format_string.format(bitwise_or).count('1')
+		if count > best_value : 
+			best_value = count 
+			team_count = 1
+		elif count == best_value:
+			team_count += 1
 
-	# determine the max knowledge value
-	max_knowledge = 0
-	team_values = {}
-	for team in teams:
-		combined_knowledge = sum(teams[team])
-		team_values[team] = combined_knowledge
-		if combined_knowledge >= max_knowledge:
-			max_knowledge = combined_knowledge
-	
-	return max_knowledge, team_values
+	print '--------- using itertools... -------------'
+	print best_value, '\n', team_count
 
-def find_top_teams(max_knowledge, team_values):
-	# determine how many teams have that max knowledge
-	top_teams = 0
-	for team in team_values:
-		if team_values[team] == max_knowledge:
-			top_teams += 1
+def compute_values_nested(n, m, people):
+	format_string = '{0:0%sb}' % m
+	best_value, team_count = 0, 0
 
-	return top_teams
+	for i in range(0, n):
+		for j in range(i + 1, n):
+			bitwise_or = int(people[i], 2) | int(people[j], 2)
+			count = format_string.format(bitwise_or).count('1')
+			if count > best_value : 
+				best_value = count 
+				team_count = 1
+			elif count == best_value:
+				team_count += 1
+
+	print '--------- using nested... -------------'
+	print best_value, '\n', team_count
 
 def main():
 	first_line = raw_input()
@@ -76,11 +76,9 @@ def main():
 
 	people = [raw_input() for _ in range(0,n)]
 	
-	max_knowledge, team_values = find_max_knowledge(n, m, people)
-	top_teams = find_top_teams(max_knowledge, team_values)
-
-	print max_knowledge
-	print top_teams
+	compute_values_itertools(n, m, people)
+	print '\n'
+	compute_values_nested(n, m, people)
 	
 
 if __name__ == '__main__':
