6b80c5a1e4fd09f6a99280fe2a1062b51badeb27
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-03-16 15:03:04 +0100

    Much faster, but still fails three testcases

--- filling_jars.py
+++ filling_jars.py
@@ -4,33 +4,25 @@
 	https://www.hackerrank.com/challenges/filling-jars
 '''
 
-def do_work(n, m, operations):
-	candy_jars = [long(0)] * n
-	
-	for operation in operations:
-		low = operations[operation][0]
-		high = operations[operation][1]
-		value = operations[operation][2]
-
-		for i in range(long(low) - 1, long(high)):
-			candy_jars[i] += long(value)
-
-	print sum(candy_jars) / n
-
 def main():
 	first_line = raw_input()
 	first_line = first_line.split(' ')
 	n = int(first_line[0]) # the number of empty candy jars (bins)
 	m = int(first_line[1]) # the number of operations  to be performed
 
-	operations = {}
+	total = 0
 	for operation in range(n):
 		try:
-			operations[operation] = raw_input().split(' ') # a=low index, b=high index, k=value
+			a, b, k = raw_input().split(' ') # a=low index, b=high index, k=value
+			a = int(a)
+			b = int(b)
+			k = int(k)
+			# fill each of the spots between b and a with the k value.
+			total = total + (b - a + 1) * k
 		except EOFError:
 			break # end of file
 
-	do_work(n, m, operations)
+	print total / n # average!
 
 if __name__ == '__main__':
 	main()
