47830986d514c51755875ca12306922d3c2033f2
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-03-19 17:55:02 -0700

    adding more to the ssh scanenr

--- ssh_scanner.sh
+++ ssh_scanner.sh
@@ -1,14 +1,23 @@
-#! /usr/bin/bash
+#!/bin/bash
 
 # scan the network for machines.
 # try to connect to each machine that is up, via ssh.
-for host in `nmap -sP 192.168.0.1/24 2> /dev/null | grep "for" | cut -d " " -f 5`; do
-  ssh -o ConnectTimeout=1 -o BatchMode=yes -o StrictHostKeyChecking=no phelan@$host
-  echo "$?"
+thisHost="$(ipconfig getifaddr en0)"
+defaultGateway="$(netstat -nr | grep '^default' | awk '{print $2}')"
+network="192.168.0.1/23"
+
+for host in `nmap -sP $network 2> /dev/null | grep "for" | cut -d " " -f 5`; do
+  if [ "$host" != "$thisHost" ] ; then
+    if [ "$host" != "$defaultGateway" ] ; then
+      nc -z $host 22
+      if [ $? != 1 ]; then
+        ssh -o ConnectTimeout=1 -o BatchMode=yes -o StrictHostKeyChecking=no phelan@$host &
+        exit 0
+      fi
+    fi
+  fi
 done
 
-#TODO: make the network an argument. 
-#TODO: make the password an argument.
-#TODO: exit on success.
-#TODO: exclude the current host from the ssh connection attempts.
+echo All hosts on the network closed to ssh.
+exit 1
 
