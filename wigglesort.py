0505479ba40cb158b90abe24652365a5f87fa9d9
Phelan Vendeville <pheven@google.com>
2017-10-30 13:00:18 -0700

    Add some perf monitoring

--- wigglesort.py
+++ wigglesort.py
@@ -1,12 +1,14 @@
 import profile
+import random
 
 # Sort an array of numbers such that s1 <= s2 >= s3 <= s4 ...
-master = [12, 22, 6, 8, 3, 7, 7, 90, 0, 134]
+# master = [12, 22, 6, 8, 3, 7, 7, 90, 0, 134]
 
-def solution_1():
+
+def solution_1(u):
   # find min & max, remove them and add them to output
   # O(N^2)
-  u = master[:]
+  u = u[:]
   s = []
   while len(u) > 0:
     low = u.index(min(u))
@@ -15,13 +17,13 @@ def solution_1():
     high = u.index(max(u))
     s.append(u[high])
     del(u[high])
-    
+  
   return s
 
-def solution_2():
+def solution_2(u):
   # sort, then remove alternatively from front and back
   # O(N ln N)
-  u = master[:]
+  u = u[:]
   s = []
   u.sort()
   while len(u) > 0:
@@ -30,13 +32,12 @@ def solution_2():
 
   return s
 
-def solution_3():
+def solution_3(u):
   # Find the median, cut the input into less-than & greater-or-equal-to the
   # median, then output alternating from the the two parts.
   # O(N)
-  u = master[:]
+  u = u[:]
   s = []
-
   u.sort()
   med = len(u) / 2
   low = u[0:med]
@@ -47,9 +48,12 @@ def solution_3():
 
   return s
 
-print solution_1()
-print solution_2()
-print solution_3()
+#print solution_1(master)
+#print solution_2(master)
+#print solution_3(master)
 
-# profile.run('print(solution_1())')
-# profile.run('print(solution_2())')
+u = random.sample(range(1, 10000), 9000) 
+profile.run('solution_1(u)')
+profile.run('solution_2(u)')
+profile.run('solution_3(u)')
+
